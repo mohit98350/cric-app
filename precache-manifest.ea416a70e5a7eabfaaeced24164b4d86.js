66adbdf4bc325e4412839c3ff87efaa0ff732be2
Mohit Mayank <65847302+mohit98350@users.noreply.github.com>
2020-10-14 22:37:10 +0530

    Updates

---
+++ precache-manifest.ea416a70e5a7eabfaaeced24164b4d86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f894a7af87aecae7c14f7f7905a9e5",
+    "url": "/cric-app/index.html"
+  },
+  {
+    "revision": "7dbabc166acab6bd62ef",
+    "url": "/cric-app/static/css/main.78d0e163.chunk.css"
+  },
+  {
+    "revision": "953f1ea3c97609abda33",
+    "url": "/cric-app/static/js/2.9b89b24d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cric-app/static/js/2.9b89b24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dbabc166acab6bd62ef",
+    "url": "/cric-app/static/js/main.f11154d2.chunk.js"
+  },
+  {
+    "revision": "5a17dd0739b76465fa9a",
+    "url": "/cric-app/static/js/runtime-main.de075f32.js"
+  },
+  {
+    "revision": "45532792eba2447ee893a61cca3e6baf",
+    "url": "/cric-app/static/media/cri.45532792.png"
+  }
+]);
